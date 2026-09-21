5b1835ceddef3935a70cedded216642ed166f2a9
KamilFuczylo <u3503@zs2lubin.pl>
2026-09-21 19:14:38 +0200

    dodawanie podręczników

---
+++ dodawanie.py
@@ -0,0 +1,31 @@
+def dodaj_podrecznik(podreczniki):
+    print("\n--- DODAWANIE PODRĘCZNIKA ---")
+
+    tytul = input("Tytuł: ")
+    klasa = input("Klasa: ")
+    przedmiot = input("Przedmiot: ")
+    stan = input("Stan (nowy/dobry/używany): ")
+
+    while True:
+        try:
+            cena = float(input("Cena (zł): "))
+
+            if cena < 0:
+                print("Cena nie może być ujemna.")
+            else:
+                break
+
+        except ValueError:
+            print("Podaj poprawną cenę, np. 25.50")
+
+    podrecznik = {
+        "tytul": tytul,
+        "klasa": klasa,
+        "przedmiot": przedmiot,
+        "stan": stan,
+        "cena": cena
+    }
+
+    podreczniki.append(podrecznik)
+
+    print("\nPodręcznik został dodany!")
